5c951d1fba61a7f14c59b4ce30120c7d7703cff2
SumedhBengale <ssbengale@gmail.com>
2021-09-22 15:16:34 +0530

    Added Bootstrap, button.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <h1>React Test Site</h1>
+      <button className="btn btn-primary">Text</button>
     </div>
   );
 }
